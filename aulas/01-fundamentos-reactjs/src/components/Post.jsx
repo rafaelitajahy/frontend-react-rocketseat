686e37cc8bd66a937ef7a91f8ceb6441ddeca1ec
rafaelitajahy <rafaelitajahy@gmail.com>
2022-11-07 20:18:30 -0300

    feat: create comment

--- aulas/01-fundamentos-reactjs/src/components/Post.jsx
+++ aulas/01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,27 +1,37 @@
+import { Comment } from './Comment';
 import styles from './Post.module.css';
 
-export function Post() {  
+export function Post() {
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://github.com/rafaelitajahy.png"/>
+          <img
+            className={styles.avatar}
+            src="https://github.com/rafaelitajahy.png"
+          />
           <div className={styles.authorInfo}>
             <strong>Rafael Itajahy</strong>
             <span>Web Developer</span>
           </div>
         </div>
 
-        <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Publicado há 1h</time>
+        <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">
+          Publicado há 1h
+        </time>
       </header>
 
       <div className={styles.content}>
-      <p>Fala galeraa 👋</p>
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-        <p><a href="">jane.design/doctorcare</a></p>
+        <p>Fala galeraa 👋</p>
         <p>
-          <a href="">#novoprojeto</a>{' '}
-          <a href="">#nlw</a>{' '}
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          <a href="">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto</a> <a href="">#nlw</a>{' '}
           <a href="">#rocketseat</a>
         </p>
       </div>
@@ -29,14 +39,18 @@ export function Post() {
       <form className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
-        <textarea
-          placeholder="Deixe um comentário"
-        />
+        <textarea placeholder="Deixe um comentário" />
 
         <footer>
           <button type="submit">Publicar</button>
         </footer>
-      </form>    
+      </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
     </article>
-  )
+  );
 }
