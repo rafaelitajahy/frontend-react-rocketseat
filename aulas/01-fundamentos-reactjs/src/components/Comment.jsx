894b5463d6eaffd827a252c919298c079f86e23b
rafaelitajahy <rafaelitajahy@gmail.com>
2022-11-08 11:06:41 -0300

    feat: create avatar component

--- aulas/01-fundamentos-reactjs/src/components/Comment.jsx
+++ aulas/01-fundamentos-reactjs/src/components/Comment.jsx
@@ -1,23 +1,28 @@
 import styles from './Comment.module.css';
 
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://github.com/rafaelitajahy.png" alt="" />
+      <Avatar
+        hasBorder={false}
+        src='https://github.com/rafaelitajahy.png'
+        alt=''
+      />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
           <header>
             <div className={styles.authorAndTime}>
               <strong>Rafael Itajahy</strong>
-              <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">
+              <time title='11 de Maio às 08:13h' dateTime='2022-05-11 08:13:00'>
                 Cerca de 1h atrás
               </time>
             </div>
 
-            <button title="Deletar comentário">
+            <button title='Deletar comentário'>
               <Trash size={24} />
             </button>
           </header>
--- aulas/01-fundamentos-reactjs/src/components/Post.jsx
+++ aulas/01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,22 +1,20 @@
 import { Comment } from './Comment';
 import styles from './Post.module.css';
+import { Avatar } from './Avatar';
 
 export function Post() {
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://github.com/rafaelitajahy.png"
-          />
+          <Avatar src='https://github.com/rafaelitajahy.png' alt='' />
           <div className={styles.authorInfo}>
             <strong>Rafael Itajahy</strong>
             <span>Web Developer</span>
           </div>
         </div>
 
-        <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">
+        <time title='11 de Maio às 08:13h' dateTime='2022-05-11 08:13:00'>
           Publicado há 1h
         </time>
       </header>
@@ -28,21 +26,21 @@ export function Post() {
           no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
         </p>
         <p>
-          <a href="">jane.design/doctorcare</a>
+          <a href=''>jane.design/doctorcare</a>
         </p>
         <p>
-          <a href="">#novoprojeto</a> <a href="">#nlw</a>{' '}
-          <a href="">#rocketseat</a>
+          <a href=''>#novoprojeto</a> <a href=''>#nlw</a>{' '}
+          <a href=''>#rocketseat</a>
         </p>
       </div>
 
       <form className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
-        <textarea placeholder="Deixe um comentário" />
+        <textarea placeholder='Deixe um comentário' />
 
         <footer>
-          <button type="submit">Publicar</button>
+          <button type='submit'>Publicar</button>
         </footer>
       </form>
 
