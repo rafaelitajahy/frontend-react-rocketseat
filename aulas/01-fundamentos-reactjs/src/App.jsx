fa6aac6d4962c6d3d55510e37f7175be2e59a890
rafaelitajahy <rafaelitajahy@gmail.com>
2022-11-09 19:09:32 -0300

    refactor: change content post

--- aulas/01-fundamentos-reactjs/src/App.jsx
+++ aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,10 +1,49 @@
-import { Header } from "./components/Header"
-import { Post } from "./components/Post"
+import { Header } from './components/Header';
+import { Post } from './components/Post';
 import { Sidebar } from './components/Sidebar';
 
 import styles from './App.module.css';
 
 import './global.css';
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/rafaelitajahy.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-10 20:00:00'),
+  },
+];
 
 function App() {
   return (
@@ -13,16 +52,20 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-            author="Rafael Itajahy" 
-            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste, earum quae. Vero a libero magni incidunt repellat consequatur, et quas aliquid. Magni ab perferendis eaque, enim blanditiis accusantium rem esse." 
-          />  
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
-  
-    </div>   
-  )
+    </div>
+  );
 }
 
-export default App
-
+export default App;
